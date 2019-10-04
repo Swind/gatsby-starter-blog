94435e3c4fa535fab4711215d90649a0a5603751
Swind <swind@cloudmosa.com>
2019-10-05 00:34:19 +0800

    Sync all posts with notion

--- src/notion/api/postService.ts
+++ src/notion/api/postService.ts
@@ -4,22 +4,18 @@ import { Article, ArticleMeta } from './types'
 import notionService from './notionService'
 import config from '../../../config'
 import console = require('console');
+import { string } from 'prop-types';
+import path from 'path'
 
-// import nodeSchedule from 'node-schedule'
-
-// let blogList: ArticleMeta[] = []
-//
-// getPosts().then(it => blogList = it).catch(e => console.log(e))
-// nodeSchedule.scheduleJob('* * * * *', async () => {
-//     console.log('update blog')
-//     blogList = await getPosts()
-// })
+import log from "loglevel"
 
 const pageId = config.blogTablePageId
 const viewId = config.blogTableViewId
 
 const postListPath = `${config.notionFolderPath}/PostList.json`
 const postFolderPath = `${config.notionFolderPath}/Posts`
+
+log.setLevel("debug")
 
 function writeFile(filePath: string, data: any) {
     return new Promise((resolve, reject) => {
@@ -33,10 +29,11 @@ function writeFile(filePath: string, data: any) {
     })
 }
 
-function readFile(filePath: string): Promise<string> {
+function readFile(filePath: string): Promise<string> | undefined {
     return new Promise((resolve, reject) => {
         if (!fs.existsSync(filePath)) {
-            resolve("[]")
+            log.info(`${filePath} is not existing`)
+            resolve(undefined)
         } else {
             fs.readFile(filePath, 'utf-8', (err, data) => {
                 if (err) {
@@ -49,40 +46,66 @@ function readFile(filePath: string): Promise<string> {
     })
 }
 
-async function getPostList(): Promise<Record<string, ArticleMeta>> {
-    const articleMetaList = await notionService.getArticleMetaList(pageId, viewId)
+async function savePostList(postList: Record<string, ArticleMeta>) {
+    await writeFile(postListPath, JSON.stringify(postList))
+}
 
+function _convertPostListToDict(articleMetaList: ArticleMeta[]): Record<string, ArticleMeta> {
     let result: Record<string, ArticleMeta> = {}
     articleMetaList.forEach(meta => {
         result[meta.id] = meta
     });
 
     return result
+
+}
+
+async function loadPostList(): Promise<Record<string, ArticleMeta>> {
+    const data: string | undefined = await readFile(postListPath)
+    if (data === undefined) {
+        return {}
+    }
+
+    const postList: ArticleMeta[] = JSON.parse(data)
+    return _convertPostListToDict(postList)
+}
+
+
+async function getPostList(): Promise<Record<string, ArticleMeta>> {
+    const postList = await notionService.getArticleMetaList(pageId, viewId)
+    return _convertPostListToDict(postList)
 }
 
 async function getPost(id: string): Promise<Article> {
     return notionService.getArticle(id)
 }
 
-async function updatePostList() {
-    const postList = await getPostList()
-    await writeFile(postListPath, JSON.stringify(postList))
+async function savePost(post: Article) {
+    const postFilePath = path.join(postFolderPath, post.meta.id + ".json")
+
+    log.info(`Save post ${post.meta.name} to ${postFilePath}`)
+    await writeFile(postFilePath, JSON.stringify(post))
 }
 
-async function updatePosts(postList: ArticleMeta[]) {
+async function updatePosts() {
+    const currentPostList = await loadPostList()
     const latestPostList = await getPostList()
-    const currentPostList = loadPostList()
 
-    postList.forEach(async (meta) => {
-        const post = await getPost(meta.id)
+    for (var postId in latestPostList) {
+        if (postId in currentPostList) {
+            const currentPostMeta = currentPostList[postId]
+            const latestPostMeta = latestPostList[postId]
 
-    })
-}
+            if (currentPostMeta.lastModifiedDate == latestPostMeta.lastModifiedDate) {
+                continue
+            }
+        }
 
-async function loadPostList(): Promise<ArticleMeta[]> {
-    const data: string = await readFile(postListPath)
-    const postList: ArticleMeta[] = JSON.parse(data)
-    return postList
+        log.info(`Getting post ${postId}`)
+        const newPost = await getPost(postId)
+        log.info(`Updating post ${newPost.meta.name} - ${postId}`)
+        await savePost(newPost)
+    }
 }
 
 async function loadPosts() {
@@ -98,13 +121,10 @@ export default {
     getPost,
 }
 
-/*
-updatePostList()
+updatePosts()
     .then(() => {
         console.log("Done !")
     })
     .catch(err => {
         console.log(err)
     })
-*/
-loadPostList()
